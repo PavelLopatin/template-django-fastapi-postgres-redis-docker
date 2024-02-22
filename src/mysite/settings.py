e4ea81661abbfe4e51c4fa2f3674981e6cab9c70
pavel <lopatyn1244@gmail.com>
2024-02-23 01:37:48 +0300

    updating: default value db host and port

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -63,8 +63,8 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", "postgres"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
-        "HOST": os.getenv("DB_HOST", "postgres"),
-        "PORT": os.getenv("DB_PORT", "postgres"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
